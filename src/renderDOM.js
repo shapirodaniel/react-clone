1b762a11d25dbcb8df6b7d0839d2a0f9b0df2e71
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-19 20:20:45 -0500

    update: component logic

--- src/renderDOM.js
+++ src/renderDOM.js
@@ -1,8 +1,9 @@
+const parser = new DOMParser();
+
 // driver func
 // should assemble components in another document
 // and swap the current doc for the new one
 export const renderDOM = DOMString => {
-	const parser = new DOMParser();
 	const newDocument = new Document();
 	const newDocFragment = parser.parseFromString(DOMString, 'text/html');
 	const newDOM = newDocFragment.querySelector('body *');
