de59100daee723ffdaffc074fd6c6a9e97ce986d
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-19 19:53:03 -0500

    refactor: component logic top-down

--- src/component.js
+++ src/component.js
@@ -1,8 +1,22 @@
 import { v4 as uuidv4 } from 'uuid';
 
+// components take a childData array and a children array
+// childData is an array of props for children
+// each prop is an object with name, payload
+
+/*
+	childData = [
+		{
+			name: string,
+			payload: any
+		},
+		...
+	]
+*/
+
 export class Component {
-	constructor(state = {}, children = []) {
-		this.state = state;
+	constructor(childData = [], children = []) {
+		this.childData = childData;
 		this.children = children;
 		this.isObserving = false;
 	}
@@ -16,17 +30,41 @@ export class Component {
 	}
 
 	// this render method gets invoked to
-	// create a doc fragment that can be passed up
+	// create a DOM subtree that can be passed up
 	// to the new document in renderDOM()
-	renderComponent() {
+	buildComponentSubtree() {
 		let wrapper = document.createElement('div');
 
 		wrapper.setAttribute('key', uuidv4());
 
 		this.children.forEach(child => {
+			// update child props
+			const newProps = this.childData.find(
+				propObject => propObject.name === child.props.name
+			);
+
+			child.props = newProps;
+
 			wrapper.appendChild(child);
 		});
 
 		return wrapper;
+	}
+
+	// this method is the hook that re-renders the component
+	// whenever its previousChildData, ie its childData
+	updateIfChildDataChanged(incomingChildData) {
+		// if child data hasn't changed, ignore it
+		if (
+			incomingChildData.every(
+				(child, idx) =>
+					JSON.stringify(child) === JSON.stringify(this.childData[idx])
+			)
+		) {
+			return;
+		}
+
+		this.childData = incomingChildData;
+		this.buildComponentSubtree();
 	}
 }
