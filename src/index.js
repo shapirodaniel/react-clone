5bae4497cbe9c13935b4342f045fb201f36bf1aa
shapirodaniel <shapirodanieladam@gmail.com>
2021-04-20 08:57:42 -0500

    add: doc fragment logic

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import { DOMManager } from './renderDOM';
 
 let componentList = [App];
 
-const DOM = new DOMManager(componentList);
+const manager = new DOMManager(componentList);
 
 const refreshDOM = () => {
-	DOM.setComponents(componentList);
+	manager.setComponents(componentList);
+
+	let newAppTree = new DocumentFragment();
+	newAppTree.appendChild(App);
+
+	document.querySelector('#root').appendChild(newAppTree);
 };
 
 setInterval(refreshDOM, 50);
